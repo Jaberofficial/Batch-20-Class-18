1ce9cc9c156f47a03e564513d33f081c7f9cefcd
Jaberofficial <jaberhossainmojumderononto02@gmail.com>
2026-04-15 22:01:53 +0600

    Javascript Conditional Statement Intro

---
+++ script.js
@@ -0,0 +1,24 @@
+
+let rahim;
+let karim;
+let rafiq;
+
+rahim = "walk";
+karim = "car";
+rafiq = "cycle";
+
+if(rahim == 'cycle'){
+    console.log("I will go to Park");
+}
+
+else if (karim == 'car'){
+    console.log("I will go to Shopping");
+}
+
+else if (rafiq == 'walk'){
+    console.log("We will watch Netflix");
+}
+
+else{
+    console.log("I will Study");
+}
